8c8cfc5a444811c93259c9ae0599591c3164ee71
권휘 <rnjsgnl108@naver.com>
2025-10-28 17:35:33 +0900

    초기 커밋.

---
+++ 초심찾기/printf()로 문자열 출력.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d + %d = %d\n", 123, 4567);
+	return 0;
+}
---
+++ 초심찾기/숫자를 문자열로 출력.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("123 + 456789\n");
+
+    return 0;
+}
