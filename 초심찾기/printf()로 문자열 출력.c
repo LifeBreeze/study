2af7cac20adbd0bedde977963f813e914730078b
LifeBreeze <149758952+LifeBreeze@users.noreply.github.com>
2025-10-28 18:33:37 +0900

    ???
    Added a comment to the first printf statement.

--- 초심찾기/printf()로 문자열 출력.c
+++ 초심찾기/printf()로 문자열 출력.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-	printf("hello world");
-	printf("hello world ÀÌ°Ç º¯°æÇÑ°Å, ¶Ç º¯°æÇÒ°ÅÀÓ");
-	printf("½ÇÇèÀÓ");
+	printf("hello world"); ë­ì•¼
+	printf("hello world ì´ê±´ ë³€ê²½í•œê±°, ë˜ ë³€ê²½í• ê±°ì„");
+	printf("ì‹¤í—˜ì„");
 	return 0;
 }
